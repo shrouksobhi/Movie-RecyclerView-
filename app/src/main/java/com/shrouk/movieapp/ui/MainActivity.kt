b1a83be6e2589f0c9e86efb411479b5b2c790997
shrouksobhi <shrouksobhi99@gmail.com>
2022-08-04 23:53:31 +0200

    MVVM,MVP

--- app/src/main/java/com/shrouk/movieapp/ui/MainActivity.kt
+++ app/src/main/java/com/shrouk/movieapp/ui/MainActivity.kt
@@ -4,54 +4,96 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.shrouk.movieapp.Api.RetrofitFactory
+import com.shrouk.movieapp.MVP.ProductsPresenter
+import com.shrouk.movieapp.MVP.ProductsView
+import com.shrouk.movieapp.MVVM.ProductsVM
 import com.shrouk.movieapp.R
 import com.shrouk.movieapp.interfaces.ProductsOnClick
 import com.shrouk.movieapp.movieAdapter.MoviesAdapter
 import com.shrouk.movieapp.movieModel.Data
-import com.shrouk.movieapp.movieModel.Movies
-import retrofit2.Call
-import retrofit2.Response
 
-class MainActivity : AppCompatActivity() , ProductsOnClick {
+class MainActivity : AppCompatActivity() , ProductsOnClick ,ProductsView {
     lateinit var recyclerview: RecyclerView
     lateinit var movieadapter: MoviesAdapter
-    private lateinit var layoutManager: GridLayoutManager
+    private lateinit var layoutManager: LinearLayoutManager
+    private lateinit var movies: ArrayList<Data>
+    val  productvm : ProductsVM by viewModels()
+   lateinit var productspresenter:ProductsPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var movies: ArrayList<Data> =  ArrayList()
+        initView()
 
-        recyclerview = findViewById(R.id.recyclviewid)
-        layoutManager = GridLayoutManager(this, 3)
-        recyclerview.layoutManager = layoutManager
-        movieadapter = MoviesAdapter(movies, this@MainActivity , this@MainActivity)
-        recyclerview.adapter = movieadapter
+        //*******MVP BLOCK *********
+        productspresenter= ProductsPresenter(this)
+        productspresenter.getProducts()
+
+        //**************
+
+        // *********MVVM block of code **********
+        // productvm.getProducts()
+        //  productvm.productsLiveData.observe(this, Observer {
+        //      movies=it
+        //     installViews()
+        //      movieadapter.notifyItemRangeInserted(movies.size , movies.size)
+        //   })
+        //  productvm.errorLiveData.observe(this, Observer {
+        //      Toast.makeText(this,it,Toast.LENGTH_SHORT).show()
+        //})
+        //***************** End of MVVM block*************
 
 
-        val retrofit = RetrofitFactory().apiInterface()
-        val call = retrofit.getData()
-        call.enqueue(object : retrofit2.Callback<Movies> {
-            override fun onResponse(call: Call<Movies>, response: Response<Movies>) {
-                movies.addAll(response.body()?.data!!)
-                movieadapter.notifyItemRangeInserted(movies.size , movies.size)
-            }
+        //******* CODE BELOW FOR USING RETROFIT TP GET PRODUCTS (NO ARCH WERE PATTERN USED)*******
 
-            override fun onFailure(call: Call<Movies>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "no internet connection", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        })
+        //   val retrofit = RetrofitFactory().apiInterface()
+        // val call = retrofit.getData()
+        //call.enqueue(object : retrofit2.Callback<Movies> {
+        //  override fun onResponse(call: Call<Movies>, response: Response<Movies>) {
+        //    movies.addAll(response.body()?.data!!)
+        //  movieadapter.notifyItemRangeInserted(movies.size , movies.size)
+
+        // override fun onFailure(call: Call<Movies>, t: Throwable) {
+       //     Toast.makeText(this@MainActivity, "no internet connection", Toast.LENGTH_SHORT)
+       //         .show()
+       //  }
+      //  })
+        //*********END OF RETROFIT BLOCK*********
 
 
     }
 
+  fun initView(){
+      recyclerview = findViewById(R.id.recyclviewid)
+
+  }
+    fun installViews(){
+        layoutManager = LinearLayoutManager(this)
+        recyclerview.layoutManager = layoutManager
+        movieadapter = MoviesAdapter(movies, this@MainActivity , this@MainActivity)
+        recyclerview.adapter = movieadapter
+    }
+
+    override fun onPause() {
+        super.onPause()
+        productvm.productsLiveData.removeObservers(this)
+        productvm.errorLiveData.removeObservers(this)
+    }
     override fun productClick(productId: Int) {
         Log.i("ProductId" , "$productId")
         startActivity(Intent(this , DetailsActivity::class.java).putExtra("productId" , productId))
     }
+
+    override fun setProducts(datalist: ArrayList<Data>) {
+        movies=datalist
+        installViews()
+    }
+
+    override fun setError(errormessage: String) {
+    Toast.makeText(this,errormessage,Toast.LENGTH_SHORT).show()    }
+
 }
